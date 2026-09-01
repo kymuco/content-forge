e445188d3d024f92b98b56ea1b89cf6dcbc98636
Ikymuco <ikymuco@gmail.com>
2026-09-02 00:02:51 +0600

    feat(pr26): expose production library index

--- src/content_forge/storage/library.py
+++ src/content_forge/storage/library.py
@@ -8,6 +8,7 @@ from content_forge.core import Project
 
 from .asset_store import AssetStore
 from .database import LibraryDatabase
+from .library_index import ProductionLibraryIndex
 from .paths import RuntimePaths
 
 
@@ -18,6 +19,7 @@ class LocalLibrary:
         self.paths = RuntimePaths.from_root(root).ensure()
         self.database = LibraryDatabase(self.paths.database).initialize()
         self.assets = AssetStore(self.paths, self.database)
+        self.index = ProductionLibraryIndex(self.database).initialize()
 
     def save_project(self, project: Project) -> Project:
         return self.database.save_project(project)
